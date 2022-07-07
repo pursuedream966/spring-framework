e11fb974893d3db4db00a84e46db1731528575fa
hyy-asus <hyy_study@163.com>
2022-07-07 16:05:33 +0800

    注解编写

---
+++ hyy-spring-test/src/main/java/org/springframework/hyy/Main.java
@@ -0,0 +1,16 @@
+package org.springframework.hyy;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.hyy.config.MyConfig;
+
+public class Main {
+	public static void main(String[] args) {
+		final AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		applicationContext.scan("org.springframework.hyy");
+		applicationContext.start();
+
+		final MyConfig bean = applicationContext.getBean(MyConfig.class);
+		System.out.println(bean);
+
+	}
+}
---
+++ hyy-spring-test/src/main/java/org/springframework/hyy/config/MyConfig.java
@@ -0,0 +1,13 @@
+package org.springframework.hyy.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: yang.hu
+ * @date: 2022/7/7 15:56
+ * @version: 1.0.0
+ */
+@Configuration
+public class MyConfig {
+	private String configName = "张三";
+}
